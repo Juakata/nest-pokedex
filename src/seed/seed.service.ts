6fd5c7e813c8fe2294544068ac010c73b4012ee7
Andoni <juakata07@gmail.com>
2023-05-07 16:34:46 -0600

    :hammer: add a axios adapter to handle http requests

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -2,19 +2,21 @@ import { Injectable } from '@nestjs/common';
 import axios, { AxiosInstance } from 'axios';
 import { PokemonResponse } from './interfaces/pokemon-response.interface';
 import { PokemonsService } from 'src/pokemons/pokemons.service';
+import { AxiosAdapter } from 'src/common/adapters/axios.adapter';
 
 @Injectable()
 export class SeedService {
-  private readonly axios: AxiosInstance = axios;
-
-  constructor(private readonly pokemonService: PokemonsService) {}
+  constructor(
+    private readonly pokemonService: PokemonsService,
+    private readonly axiosAdapter: AxiosAdapter,
+  ) {}
 
   async runSeed() {
-    const { data } = await this.axios.get<PokemonResponse>(
+    const { results } = await this.axiosAdapter.get<PokemonResponse>(
       'https://pokeapi.co/api/v2/pokemon?limit=1000',
     );
     await this.pokemonService.removeAll();
-    const pokemons = data.results.map((pokemon) => {
+    const pokemons = results.map((pokemon) => {
       const { name, url } = pokemon;
       const urlArr = url.split('/');
       const no: number = +urlArr[6];
